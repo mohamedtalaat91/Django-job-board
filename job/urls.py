911bc38526d8684269a2cbf98d48a2c3347220b2
mohamedtalaat91 <147813555+mohamedtalaat91@users.noreply.github.com>
2024-07-28 17:11:32 +0300

    add job views and urls

---
+++ job/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path , include
+from . import views
+
+urlpatterns = [
+    path('', views.job_list, name='job_list'),
+    path('<int:id>/', views.job_detail, name='job_detail'),
+]
